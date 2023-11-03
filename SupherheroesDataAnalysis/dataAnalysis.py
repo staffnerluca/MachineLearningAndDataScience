429cbcc00fcb2d4fa6f33b2a3f696d60c2007e3b
staffnerluca <113279240+staffnerluca@users.noreply.github.com>
2023-11-03 10:18:55 +0100

    Update dataAnalysis.py

--- SupherheroesDataAnalysis/dataAnalysis.py
+++ SupherheroesDataAnalysis/dataAnalysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 
+
 def preprocessGSMOld(df):
     li = df["GSM"]
     for i in range(len(li)):
@@ -13,42 +14,79 @@ def preprocessGSMOld(df):
         df["GSM"] = li
         print(df["GSM"])
         return df
-    
+
+
 def preprocessGSM(df):
-    return [0 if isinstance(i, float) else 1 if i.startswith("H") else 2 if i.startswith("B") else 3 for i in df]
+    return [0 if isinstance(x, float) else 1 if x.startswith("H") else 2 if x.startswith("B") else 3 for x in df["GSM"]]
+    return df["GSM"].apply(lambda x: 0 if isinstance(x, float) else 1 if x.startswith("H") else 2 if x.startswith("B") else 3)
+
 
 def preprocessSex(df):
     #NaN is float
-    return [0 if isinstance(i, float) else 1 if i.startswith("M") else 2 if i.startswith("F") else 3 if i.startswith("G") else 4 if i.startswith("T") else 5 for i in df]
+    return df["SEX"].apply(lambda x: 0 if isinstance(x, float) else 1 if x.startswith("M") else 2 if x.startswith("F") else 3 if x.startswith("G") else 4 if x.startswith("T") else 5)
+
 
 def preprocessAlign(df):
-    return [0 if isinstance(i, float) else 1 if i.startswith("G") else 2 if i.startswith("B") else 3 if i.startswith("N") else 4 if i.startswith("R") else 5 for i in df]
+    return df["ALIGN"].apply(lambda x: 0 if isinstance(x, float) else 1 if x.startswith("G") else 2 if x.startswith("B") else 3 if x.startswith("N") else 4 if x.startswith("R") else 5)
+
 
 def preprocessID(id):
-    return [0 if isinstance(i, float) else 1 if i.startswith("P") else 2 if i.startswith("S") else 3 if i.startswith("U") else 4 for i in id]
+     return df["ID"].apply(lambda x: 0 if isinstance(x, float) else 1 if x.startswith("P") else 2 if x.startswith("S") else 3 if x.startswith("U") else 4)
+
 
 def preprocessAlive(df):
-    return [0 if isinstance(i, float) else 1 if i.startswith("A") else 2 for i in df]
+    return df["ALIVE"].apply(lambda x: 0 if isinstance(x, float) else 1 if x.startswith("L") else 2)
+
 
 def preprocessData(df):
-    df["GSM"] = preprocessGSM(df["GSM"])
-    df["SEX"] = preprocessSex(df["SEX"])
-    df["ALIGN"] = preprocessAlign(df["ALIGN"])
-    df["ID"] = preprocessID(df["ID"])
-    df["ALIVE"] = preprocessAlive(df["ALIVE"])
+    df["GSM"] = preprocessGSM(df)
+    df["SEX"] = preprocessSex(df)
+    df["ALIGN"] = preprocessAlign(df)
+    df["ID"] = preprocessID(df)
+    df["ALIVE"] = preprocessAlive(df)
     return df
+
 
 def getNumebrOfQueerCharacters(df):
     count = len(list(filter(lambda x: x == 1 or x == 2, df["GSM"])))
-    #print("Number of sexual minority members: "+str(count))
-    sex = df["SEX"]
     count += len(list(filter(lambda x: x == 3 or x == 4, df["SEX"])))
     return count
 
-def getPercentOfQueerCharactersWihtAlignmentA(df, a):
-    num = getNumebrOfQueerCharacters(df)
-    alignA = len(list(filter(lambda x: x == a, df["ALIGN"])))
-    return alignA / num
+
+def isQueer(el):
+    if el["GSM"] == 1 or el["GSM"] == 2:
+        return True
+    if el["SEX"] == 3 or el["SEX"] == 4:
+        return True
+    return False
+    
+
+def getPercentOfCharactersWithAlignment(df, a):
+    alignA = df[df["ALIGN"] == a]
+    alignA = alignA[(alignA["GSM"] != 1) & (alignA["GSM"] != 2) & (alignA["SEX"] != 3) & (alignA["SEX"] != 4)]
+    # 
+    return len(alignA) / len(df)
+
+
+def getPercentOfQueerCharactersWithAlignmentA(df, a):
+    alignA = df[df["ALIGN"] == a]
+    alignA = alignA[(alignA["GSM"] == 1) | (alignA["GSM"] == 2) | (alignA["SEX"] == 3) | (alignA["SEX"] == 4)]
+    return len(alignA) / getNumebrOfQueerCharacters(df)
+
+
+def getDeathProbablityOfNotQueerCharacters(df):
+    aliveList = df[df["ALIVE"] == 1]
+    print(len(aliveList))
+    aliveList = aliveList[(aliveList["GSM"] != 1) & (aliveList["GSM"] != 2) & (aliveList["SEX"] != 3) & (aliveList["SEX"] != 4)]
+    print(len(aliveList))
+    return len(aliveList) / len(df)
+
+
+def getDeathProbabilityForQueerCharacters(df):
+    aliveList = df[df["ALIVE"] == 1]
+    aliveList = aliveList[(aliveList["GSM"] == 1) | (aliveList["GSM"] == 2) | (aliveList["SEX"] == 3) | (aliveList["SEX"] == 4)]
+    return len(aliveList) / getNumebrOfQueerCharacters(df)
+
 
 def createAlignmentHistorgram(df):
     #queerAlign = list(filter(lambda x: x["GSM"] == 1 or x["GSM"] == 2, df))
@@ -70,16 +108,23 @@ def createAlignmentHistorgram(df):
     )
     figQueer.show()
 
+
 df = pd.read_csv("dc-wikia-data.csv")
 
-preprocessData(df)
+df = preprocessData(df)
 
-print(df)
+print(df["SEX"])
+
+#createAlignmentHistorgram(df)
 
 print("########Report about Queer Representation########")
 
 print("Total number of queer characters: "+str(getNumebrOfQueerCharacters(df)))
-print("Percent of Evil queer Characters: "+str(getPercentOfQueerCharactersWihtAlignmentA(df, 2)))
-print("Percent of Good queer Characters: "+str(getPercentOfQueerCharactersWihtAlignmentA(df, 1)))
+print("Percent of evil characters: " + str(getPercentOfCharactersWithAlignment(df, 2)))
+print("Percent of Evil queer Characters: "+str(round(getPercentOfQueerCharactersWithAlignmentA(df, 2), 3)*100)+"%")
+print("VS the percent of non queer evil characters: ")
+print("Percent of Good queer Characters: "+str(round(getPercentOfQueerCharactersWithAlignmentA(df, 1), 2)*100)+"%")
 
-createAlignmentHistorgram(df)
+
+print("Probability for a queer Character to stay alive: "+str(round(getDeathProbabilityForQueerCharacters(df), 2)*100)+"%")
+print("Probability for a non queer Character to stay alive: "+str(round(getDeathProbablityOfNotQueerCharacters(df), 2)*100)+"%")
